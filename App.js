77f93b0de5f66cfe4424cdbd676c08a44d52a407
Rehmanplays54 <Rehmanplays54@gmail.com>
2024-08-05 22:40:55 +0500

    sat

--- App.js
+++ App.js
@@ -36,7 +36,9 @@ function App() {
     <DataProvider>
     <NavigationContainer>
       <Stack.Navigator screenOptions={{ headerShown: false }}>
-      
+      <Stack.Screen name='SplashScreen' component={SplashScreen} />
+<Stack.Screen name='Start' component={Start} />
+<Stack.Screen name='Login' component={Login} /> 
         <Stack.Screen name='Home' component={Home} />
         <Stack.Screen name='Details' component={Details} />
         <Stack.Screen name='Basket' component={Basket} />
@@ -50,9 +52,7 @@ function App() {
   );
 }
 
-{/* <Stack.Screen name='SplashScreen' component={SplashScreen} />
-<Stack.Screen name='Start' component={Start} />
-<Stack.Screen name='Login' component={Login} /> */}
+
 
 
 const styles = StyleSheet.create({
